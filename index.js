9b2a7af8ae34cd45c338878af833b1b33f806809
mhtoin <mhtoin@utu.fi>
2020-08-03 18:57:04 +0300

    Working individual GET, DELETE

--- index.js
+++ index.js
@@ -3,42 +3,31 @@ const app = express()
 
 app.use(express.json())
 
-let phonebook = [
-  {
-    "persons":[
-      { 
-        "name": "Arto Hellas", 
-        "number": "040-123456",
-        "id": 1
-      },
-      { 
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      { 
-        "name": "Dan Abramov", 
-        "number": "12-43-234345",
-        "id": 3
-      },
-      { 
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122",
-        "id": 4
-      }
-    ]
+let persons = [
+  { 
+    "name": "Arto Hellas", 
+    "number": "040-123456",
+    "id": 1
+  },
+  { 
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523",
+    "id": 2
+   },
+  { 
+    "name": "Dan Abramov", 
+    "number": "12-43-234345",
+    "id": 3
+  },
+  { 
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122",
+    "id": 4
   }
 ]
 
 app.get('/api/persons', (req, res) => {
-  res.json(phonebook)
-})
-
-app.get('/info', (req, res) => {
-    let personAmount = phonebook[0].persons.length
-    let time = new Date()
-    res.send(`<p>Phonebook has info for ${personAmount} people</p><p>${time}</p>`)
-
+  res.json(persons)
 })
 
 const generateId = () => {
@@ -47,6 +36,48 @@ const generateId = () => {
     : 0
   return maxId + 1
 }
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  console.log(id)
+  const person = persons.find(person => person.id === id)
+  console.log(person)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  console.log("Before", persons)
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  console.log("after", persons)
+
+  response.status(204).end()
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
